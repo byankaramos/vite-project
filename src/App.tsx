325ae40677bae99384706107a8f0a3e5076f198f
Byanka Ramos <byanka.thamilli.santos@gmail.com>
2023-08-14 16:47:10 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './Components/Button'
+
+export function App() {
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
+  )
+}
